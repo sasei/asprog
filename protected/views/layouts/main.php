446e15db679068d72741273740978d14602674dc
sasei <e.s.sanchez@gmail.com>
2015-06-16 11:23:19 -0500

    v1.0.0.1
    se agregan js

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -57,7 +57,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); echo " ".$this->pageTitle=Yii::app()->name;?>. <br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
